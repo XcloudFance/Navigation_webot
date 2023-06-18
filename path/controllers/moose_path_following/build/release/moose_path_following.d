33a08aea48e0c09a2c8e83641a9841d7b77ed023
LanceLot <34064977+XcloudFance@users.noreply.github.com>
2023-06-18 19:16:05 +0800

    navigation set

---
+++ path/controllers/moose_path_following/build/release/moose_path_following.d
@@ -0,0 +1,8 @@
+build/release/moose_path_following.o: moose_path_following.c \
+ D:\Webots/include/controller/c/webots/compass.h \
+ D:\Webots/include/controller/c/webots/types.h \
+ D:\Webots/include/controller/c/webots/gps.h \
+ D:\Webots/include/controller/c/webots/keyboard.h \
+ D:\Webots/include/controller/c/webots/motor.h \
+ D:\Webots/include/controller/c/webots/robot.h \
+ D:\Webots/include/controller/c/webots/nodes.h
